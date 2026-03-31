fb11822362b36d11423061e47bc2474f76474c95
tacascer <trandangtrithanh2000@gmail.com>
2026-04-01 00:39:35 +0100

    feat: configure firefox to follow system theme

--- modules/features/firefox.nix
+++ modules/features/firefox.nix
@@ -1,5 +1,15 @@
 {...}: {
   flake.nixosModules.firefox = {
-    programs.firefox.enable = true;
+    programs.firefox = {
+      enable = true;
+      preferences = {
+        # Use the default theme that follows system light/dark preference
+        "extensions.activeThemeID" = "default-theme@mozilla.org";
+        # Follow system color scheme for web content
+        "layout.css.prefers-color-scheme.content-override" = 2;
+        # Enable KDE/Plasma integration for file dialogs
+        "widget.use-xdg-desktop-portal.file-picker" = 1;
+      };
+    };
   };
 }
