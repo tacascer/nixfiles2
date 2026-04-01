002e3a90357b413eb5df5c790dad1ad65db50894
tacascer <trandangtrithanh2000@gmail.com>
2026-04-01 01:06:53 +0100

    fix(nvim): correct lua keymap actions for nvf compatibility
    nvf pastes lua=true actions directly as vim.keymap.set() arguments,
    not wrapped in functions. Fix by:
    - Using function references (no parens) for simple calls
    - Wrapping statements and complex expressions in function() ... end

--- modules/features/nvim.nix
+++ modules/features/nvim.nix
@@ -121,7 +121,7 @@
                   # --- Buffers ---
                   { key = "<S-h>"; mode = "n"; action = "<cmd>BufferLineCyclePrev<CR>"; desc = "Previous buffer"; }
                   { key = "<S-l>"; mode = "n"; action = "<cmd>BufferLineCycleNext<CR>"; desc = "Next buffer"; }
-                  { key = "<leader>bd"; mode = "n"; action = "require('mini.bufremove').delete(0, false)"; desc = "Delete buffer"; lua = true; }
+                  { key = "<leader>bd"; mode = "n"; action = "function() require('mini.bufremove').delete(0, false) end"; desc = "Delete buffer"; lua = true; }
                   { key = "<leader>bo"; mode = "n"; action = "<cmd>BufferLineCloseOthers<CR>"; desc = "Close other buffers"; }
                   { key = "<leader>bp"; mode = "n"; action = "<cmd>BufferLineTogglePin<CR>"; desc = "Toggle pin"; }
                   { key = "<leader>bb"; mode = "n"; action = "<cmd>Telescope buffers<CR>"; desc = "Switch buffer"; }
@@ -144,15 +144,15 @@
                   { key = "gr"; mode = "n"; action = "<cmd>Telescope lsp_references<CR>"; desc = "Go to references"; }
                   { key = "gI"; mode = "n"; action = "<cmd>Telescope lsp_implementations<CR>"; desc = "Go to implementation"; }
                   { key = "gy"; mode = "n"; action = "<cmd>Telescope lsp_type_definitions<CR>"; desc = "Go to type definition"; }
-                  { key = "K"; mode = "n"; action = "vim.lsp.buf.hover()"; desc = "Hover docs"; lua = true; }
-                  { key = "gK"; mode = "n"; action = "vim.lsp.buf.signature_help()"; desc = "Signature help"; lua = true; }
-                  { key = "<leader>ca"; mode = "n"; action = "vim.lsp.buf.code_action()"; desc = "Code action"; lua = true; }
-                  { key = "<leader>ca"; mode = "v"; action = "vim.lsp.buf.code_action()"; desc = "Code action"; lua = true; }
-                  { key = "<leader>cr"; mode = "n"; action = "vim.lsp.buf.rename()"; desc = "Rename symbol"; lua = true; }
-                  { key = "<leader>cf"; mode = "n"; action = "vim.lsp.buf.format()"; desc = "Format document"; lua = true; }
-                  { key = "<leader>cd"; mode = "n"; action = "vim.diagnostic.open_float()"; desc = "Line diagnostics"; lua = true; }
-                  { key = "]d"; mode = "n"; action = "vim.diagnostic.goto_next()"; desc = "Next diagnostic"; lua = true; }
-                  { key = "[d"; mode = "n"; action = "vim.diagnostic.goto_prev()"; desc = "Previous diagnostic"; lua = true; }
+                  { key = "K"; mode = "n"; action = "vim.lsp.buf.hover"; desc = "Hover docs"; lua = true; }
+                  { key = "gK"; mode = "n"; action = "vim.lsp.buf.signature_help"; desc = "Signature help"; lua = true; }
+                  { key = "<leader>ca"; mode = "n"; action = "vim.lsp.buf.code_action"; desc = "Code action"; lua = true; }
+                  { key = "<leader>ca"; mode = "v"; action = "vim.lsp.buf.code_action"; desc = "Code action"; lua = true; }
+                  { key = "<leader>cr"; mode = "n"; action = "vim.lsp.buf.rename"; desc = "Rename symbol"; lua = true; }
+                  { key = "<leader>cf"; mode = "n"; action = "vim.lsp.buf.format"; desc = "Format document"; lua = true; }
+                  { key = "<leader>cd"; mode = "n"; action = "vim.diagnostic.open_float"; desc = "Line diagnostics"; lua = true; }
+                  { key = "]d"; mode = "n"; action = "vim.diagnostic.goto_next"; desc = "Next diagnostic"; lua = true; }
+                  { key = "[d"; mode = "n"; action = "vim.diagnostic.goto_prev"; desc = "Previous diagnostic"; lua = true; }
 
                   # --- Diagnostics (Trouble) ---
                   { key = "<leader>xx"; mode = "n"; action = "<cmd>Trouble diagnostics toggle<CR>"; desc = "Document diagnostics"; }
@@ -161,34 +161,34 @@
                   { key = "<leader>xQ"; mode = "n"; action = "<cmd>Trouble qflist toggle<CR>"; desc = "Quickfix list"; }
 
                   # --- Debug (DAP) ---
-                  { key = "<leader>db"; mode = "n"; action = "require('dap').toggle_breakpoint()"; desc = "Toggle breakpoint"; lua = true; }
-                  { key = "<leader>dc"; mode = "n"; action = "require('dap').continue()"; desc = "Continue"; lua = true; }
-                  { key = "<leader>di"; mode = "n"; action = "require('dap').step_into()"; desc = "Step into"; lua = true; }
-                  { key = "<leader>do"; mode = "n"; action = "require('dap').step_over()"; desc = "Step over"; lua = true; }
-                  { key = "<leader>dO"; mode = "n"; action = "require('dap').step_out()"; desc = "Step out"; lua = true; }
-                  { key = "<leader>dt"; mode = "n"; action = "require('dap').terminate()"; desc = "Terminate"; lua = true; }
-                  { key = "<leader>du"; mode = "n"; action = "require('dapui').toggle()"; desc = "Toggle DAP UI"; lua = true; }
+                  { key = "<leader>db"; mode = "n"; action = "require('dap').toggle_breakpoint"; desc = "Toggle breakpoint"; lua = true; }
+                  { key = "<leader>dc"; mode = "n"; action = "require('dap').continue"; desc = "Continue"; lua = true; }
+                  { key = "<leader>di"; mode = "n"; action = "require('dap').step_into"; desc = "Step into"; lua = true; }
+                  { key = "<leader>do"; mode = "n"; action = "require('dap').step_over"; desc = "Step over"; lua = true; }
+                  { key = "<leader>dO"; mode = "n"; action = "require('dap').step_out"; desc = "Step out"; lua = true; }
+                  { key = "<leader>dt"; mode = "n"; action = "require('dap').terminate"; desc = "Terminate"; lua = true; }
+                  { key = "<leader>du"; mode = "n"; action = "require('dapui').toggle"; desc = "Toggle DAP UI"; lua = true; }
 
                   # --- File Explorer ---
                   { key = "<leader>e"; mode = "n"; action = "<cmd>Neotree toggle<CR>"; desc = "Toggle file explorer"; }
                   { key = "<leader>E"; mode = "n"; action = "<cmd>Neotree focus<CR>"; desc = "Focus file explorer"; }
 
                   # --- Git ---
-                  { key = "<leader>gg"; mode = "n"; action = "require('toggleterm.terminal').Terminal:new({cmd='lazygit', hidden=true, direction='float'}):toggle()"; desc = "Lazygit"; lua = true; }
-                  { key = "<leader>ghs"; mode = "n"; action = "require('gitsigns').stage_hunk()"; desc = "Stage hunk"; lua = true; }
-                  { key = "<leader>ghr"; mode = "n"; action = "require('gitsigns').reset_hunk()"; desc = "Reset hunk"; lua = true; }
-                  { key = "<leader>ghp"; mode = "n"; action = "require('gitsigns').preview_hunk()"; desc = "Preview hunk"; lua = true; }
-                  { key = "<leader>ghb"; mode = "n"; action = "require('gitsigns').blame_line()"; desc = "Blame line"; lua = true; }
-                  { key = "]h"; mode = "n"; action = "require('gitsigns').next_hunk()"; desc = "Next hunk"; lua = true; }
-                  { key = "[h"; mode = "n"; action = "require('gitsigns').prev_hunk()"; desc = "Previous hunk"; lua = true; }
+                  { key = "<leader>gg"; mode = "n"; action = "function() require('toggleterm.terminal').Terminal:new({cmd='lazygit', hidden=true, direction='float'}):toggle() end"; desc = "Lazygit"; lua = true; }
+                  { key = "<leader>ghs"; mode = "n"; action = "require('gitsigns').stage_hunk"; desc = "Stage hunk"; lua = true; }
+                  { key = "<leader>ghr"; mode = "n"; action = "require('gitsigns').reset_hunk"; desc = "Reset hunk"; lua = true; }
+                  { key = "<leader>ghp"; mode = "n"; action = "require('gitsigns').preview_hunk"; desc = "Preview hunk"; lua = true; }
+                  { key = "<leader>ghb"; mode = "n"; action = "require('gitsigns').blame_line"; desc = "Blame line"; lua = true; }
+                  { key = "]h"; mode = "n"; action = "require('gitsigns').next_hunk"; desc = "Next hunk"; lua = true; }
+                  { key = "[h"; mode = "n"; action = "require('gitsigns').prev_hunk"; desc = "Previous hunk"; lua = true; }
 
                   # --- UI Toggles ---
-                  { key = "<leader>un"; mode = "n"; action = "vim.o.number = not vim.o.number"; desc = "Toggle line numbers"; lua = true; }
-                  { key = "<leader>ur"; mode = "n"; action = "vim.o.relativenumber = not vim.o.relativenumber"; desc = "Toggle relative numbers"; lua = true; }
-                  { key = "<leader>uw"; mode = "n"; action = "vim.o.wrap = not vim.o.wrap"; desc = "Toggle word wrap"; lua = true; }
-                  { key = "<leader>us"; mode = "n"; action = "vim.o.spell = not vim.o.spell"; desc = "Toggle spell check"; lua = true; }
-                  { key = "<leader>ud"; mode = "n"; action = "vim.diagnostic.enable(not vim.diagnostic.is_enabled())"; desc = "Toggle diagnostics"; lua = true; }
-                  { key = "<leader>uc"; mode = "n"; action = "vim.o.conceallevel = vim.o.conceallevel == 0 and 2 or 0"; desc = "Toggle conceal"; lua = true; }
+                  { key = "<leader>un"; mode = "n"; action = "function() vim.o.number = not vim.o.number end"; desc = "Toggle line numbers"; lua = true; }
+                  { key = "<leader>ur"; mode = "n"; action = "function() vim.o.relativenumber = not vim.o.relativenumber end"; desc = "Toggle relative numbers"; lua = true; }
+                  { key = "<leader>uw"; mode = "n"; action = "function() vim.o.wrap = not vim.o.wrap end"; desc = "Toggle word wrap"; lua = true; }
+                  { key = "<leader>us"; mode = "n"; action = "function() vim.o.spell = not vim.o.spell end"; desc = "Toggle spell check"; lua = true; }
+                  { key = "<leader>ud"; mode = "n"; action = "function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end"; desc = "Toggle diagnostics"; lua = true; }
+                  { key = "<leader>uc"; mode = "n"; action = "function() vim.o.conceallevel = vim.o.conceallevel == 0 and 2 or 0 end"; desc = "Toggle conceal"; lua = true; }
                 ];
 
                 statusline.lualine.enable = true;
