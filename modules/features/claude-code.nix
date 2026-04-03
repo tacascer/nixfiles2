98320323413c3d695cc34acf4779c06ab022a18c
tacascer <trandangtrithanh2000@gmail.com>
2026-04-03 03:31:32 +0100

    fix(claude-code): use nodejs reference

--- modules/features/claude-code.nix
+++ modules/features/claude-code.nix
@@ -32,16 +32,16 @@
         settings = {
           includeCoAuthoredBy = false;
           enabledPlugins = {
-            "claude-mem@thedotmack" = false;
-            "superpowers@claude-plugins-official" = false;
+            # "claude-mem@thedotmack" = false;
+            # "superpowers@claude-plugins-official" = false;
             "claude-md-management@claude-plugins-official" = true;
             "oh-my-claudecode@omc" = true;
           };
           extraKnownMarketplaces = {
-            thedotmack.source = {
-              source = "github";
-              repo = "thedotmack/claude-mem";
-            };
+            # thedotmack.source = {
+            #   source = "github";
+            #   repo = "thedotmack/claude-mem";
+            # };
             omc.source = {
               source = "git";
               url = "https://github.com/Yeachan-Heo/oh-my-claudecode.git";
@@ -53,13 +53,13 @@
           };
           statusLine = {
             type = "command";
-            command = "node ${inputs.oh-my-claudecode}/dist/hud/index.js";
+            command = "${lib.getExe pkgs.nodejs} ${inputs.oh-my-claudecode}/dist/hud/index.js";
           };
         };
         pluginDirs = [
           "${inputs.oh-my-claudecode}"
-          "${inputs.claude-mem}/plugin"
-          "${inputs.superpowers}"
+          # "${inputs.claude-mem}/plugin"
+          # "${inputs.superpowers}"
           "${inputs.claude-plugins-official}/plugins/claude-md-management"
         ];
         envDefault = {
