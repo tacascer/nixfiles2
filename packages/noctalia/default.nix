81932b63fa408db0a8584a06fc64a0e3d2aab72d
tacascer <trandangtrithanh2000@gmail.com>
2026-04-22 21:48:02 +0100

    omx(team): auto-checkpoint worker-2 [3]

--- packages/noctalia/default.nix
+++ packages/noctalia/default.nix
@@ -1,5 +1,10 @@
 { pkgs, inputs, ... }:
-inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
-  inherit pkgs;
-  settings = (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings;
-}
+if pkgs.stdenv.hostPlatform.isDarwin then
+  pkgs.runCommandLocal "noctalia-unsupported-on-darwin" {
+    meta.platforms = pkgs.lib.platforms.linux;
+  } "mkdir -p $out"
+else
+  inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
+    inherit pkgs;
+    settings = (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings;
+  }
