aab1ff83d557729304d5436ba1a5ec2262ff0243
tacascer <trandangtrithanh2000@gmail.com>
2026-09-08 20:50:31 +0100

    fix: remove bazelrc management

--- modules/home/bazel.nix
+++ modules/home/bazel.nix
@@ -6,11 +6,7 @@ let
       pkgs._1password-cli
       pkgs.jq
     ];
-    text = builtins.readFile ./bazel-buildbuddy-credential-helper.sh;
   };
 in
 {
-  home.file.".bazelrc".text = ''
-    common --config=remote-linux --credential_helper=remote.buildbuddy.io=${buildBuddyCredentialHelper}/bin/bazel-buildbuddy-credential-helper
-  '';
 }
